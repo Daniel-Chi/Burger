8ea5104e1b99ca3be352c815ca99626efcf4ff80
Daniel Chi <danielhchi@gmail.com>
2019-05-16 16:13:58 -0400

    added clearDB and clearDB config

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,11 @@
 //this file sets up and exports MySQL database connection
 
 let mysql = require("mysql");
-
 let connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "root",
-    database: "burgers_db"
+    host: "us-cdbr-iron-east-02.cleardb.net",
+    user: "bf8be166cf67ab",
+    password: "428e9d75",
+    database: "heroku_bdbd6fb3e3406ba"
 });
 
 connection.connect(function (err) {
