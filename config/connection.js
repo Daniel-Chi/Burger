c7b409b3227c5c210d0ce5b1a704a5bcf72c8412
Daniel Chi <danielhchi@gmail.com>
2019-05-16 16:18:24 -0400

    connection uses localhost, or clearDB if available

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,23 @@
 //this file sets up and exports MySQL database connection
 
 let mysql = require("mysql");
+
 let connection = mysql.createConnection({
-    host: "us-cdbr-iron-east-02.cleardb.net",
-    user: "bf8be166cf67ab",
-    password: "428e9d75",
-    database: "heroku_bdbd6fb3e3406ba"
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "root",
+    database: "burgers_db"
 });
+
+if (process.env.PORT) {
+    connection = mysql.createConnection({
+        host: "us-cdbr-iron-east-02.cleardb.net",
+        user: "bf8be166cf67ab",
+        password: "428e9d75",
+        database: "heroku_bdbd6fb3e3406ba"
+    });
+}
 
 connection.connect(function (err) {
     if (err) {
